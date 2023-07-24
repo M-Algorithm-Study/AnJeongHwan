11b63f92508667fe75572569917953fc10b068fe
jeonghwan <ajh5251@gmail.com>
2023-07-24 17:30:53 +0900

    [ADD] 4주차

---
+++ Week04/백준_20920_영단어암기는괴로워.py
@@ -0,0 +1,15 @@
+# 영단어 암기는 괴로워
+import sys
+from collections import defaultdict
+input = sys.stdin.readline
+N, M = map(int, input().split())
+words = defaultdict(int)
+for _ in range(N):
+    word = input().rstrip()
+    if len(word) >= M:
+        words[word] += 1
+
+words = sorted(words.items(), key=lambda x: (-x[1], -len(x[0]), x[0]))
+
+for i in words:
+    print(i[0])
