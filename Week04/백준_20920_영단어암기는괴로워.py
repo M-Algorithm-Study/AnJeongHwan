e216bced68c3a093decb7046199cc4302854f30f
jeonghwan <ajh5251@gmail.com>
2023-07-25 09:49:43 +0900

    [UPDATE] 4주차

--- Week04/백준_20920_영단어암기는괴로워.py
+++ Week04/백준_20920_영단어암기는괴로워.py
@@ -6,10 +6,10 @@ N, M = map(int, input().split())
 words = defaultdict(int)
 for _ in range(N):
     word = input().rstrip()
-    if len(word) >= M:
-        words[word] += 1
+    if len(word) >= M:  # 단어 길이가 M 이상
+        words[word] += 1  # {단어:빈도}
 
 words = sorted(words.items(), key=lambda x: (-x[1], -len(x[0]), x[0]))
-
+# -x[1] 빈도수 /  -len(x[0]) 단어 길이 길이의 내림차순 / x[0] 사전 순
 for i in words:
     print(i[0])
--- Week04/백준_2579_계단오르기.py
+++ Week04/백준_2579_계단오르기.py
@@ -9,13 +9,15 @@ dp = [0] * (N)
 
 if len(stair) <= 2:
     print(sum(stair))
-else:
-    dp[0] = stair[0]
-    dp[1] = stair[0] + stair[1]
-    dp[2] = max(stair[0] + stair[2], stair[1] + stair[2])
+else:  # 계단이 4개 이상일 때
+    dp[0] = stair[0]  # 첫번째 계단(첫번째 밟는 경우)
+    dp[1] = stair[0] + stair[1]  # 두번째 계단(무조건 첫번째랑 두번째 계단 밟는 경우 최대)
+    dp[2] = max(stair[0] + stair[2], stair[1] + stair[2])  # 세번째 계단()
 
-    for i in range(3, N):
+    for i in range(3, N):  # 네번째 계단 부터 dp 최대값 구하기
         dp[i] = max(dp[i - 3] + stair[i - 1] + stair[i], dp[i - 2] + stair[i])
+        # i번째 계단을 오를 때
+        # 전(n-1)계단과 전전전(n-3)계단을 밟고 오른 경우 vs 전전(n-2)계단을 밟고 오른 경우
     print(dp[N - 1])
 
 
