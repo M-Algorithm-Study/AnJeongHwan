41d95987b442726f833e3f73a030955ca8ce8e97
jeonghwan <ajh5251@gmail.com>
2023-09-18 17:20:15 +0900

    [11주차]

---
+++ Week10/a.py
@@ -0,0 +1,30 @@
+import sys
+sys.stdin = open("input.txt", "r")
+input = sys.stdin.readline
+
+# 최소 길이 출력, 불가능 하면 0 출력
+N, S = map(int, input().split())
+sequence = list(map(int, input().split()))
+
+min_length = N+1
+start, end = 0, 0
+partial_sum = 0
+
+
+while True:
+    if partial_sum < S:
+        if end == N:
+            break
+        partial_sum += sequence[end]
+        end += 1
+    elif partial_sum >= S:
+        if min_length > end - start:
+            min_length = end - start
+        partial_sum -= sequence[start]
+        start += 1
+
+
+if min_length != N+1:
+    print(min_length)
+else:
+    print(0)
---
+++ Week11/백준_5555.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+t = input()
+cnt = 0
+for _ in range(int(input())):
+    s = input()
+    if t in s*2:
+        cnt += 1
+print(cnt)
