7596a5bd5c2a26393e10573ad53cf3de17e38cf9
jeonghwan <ajh5251@gmail.com>
2023-11-06 04:12:11 +0900

    [18주차]

--- Week18/백준_13549_숨바꼭질_3.py
+++ Week18/백준_13549_숨바꼭질_3.py
@@ -16,10 +16,11 @@ def bfs(start):
             print(visited[cur])
             return
         
-        next = [2*cur, cur-1, cur+1]
+        next = [2*cur, cur+1, cur-1]
 
         for i in next:
-            if 0 <= i < 100001 and visited[i] == -1: # i가 범위 안에 있고 방문하지 않았다면
+            if 0 <= i < 100001 and (visited[i] == -1 or visited[i] > visited[cur]):
+            # i가 범위 안에 있고 방문하지 않았거나 기존 방문보다 더 빠르게 방문한 경우 (4>6 가는 경우: cur+1, cur-1일 때와  cur-1, cur+1 일 때 결과가 달라짐. 4 5 6 과 4 3 6 시간 차이)
 
                 if i == 2*cur:
                     visited[i] =  visited[cur]  # 2배한 경우에는 0초 소요
